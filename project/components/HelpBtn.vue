77502dbcd06095d7ebb0d44518e12bb0d2ea54d9
shiba mitsue <mitue.okamoto@gmail.com>
2022-03-07 12:55:55 +0900

    feat: add version in header

---
+++ project/components/HelpBtn.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="flex items-center items-center"
+    @click="isHelp = !isHelp, isDialog = true"
+  >
+    <TheIcon
+      :path="mdiHelpCircleOutline"
+      size="20px"
+      class="mr-1"
+    />
+    <span>使い方</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { mdiHelpCircleOutline } from '@mdi/js'
+import { useIsHelp, useIsDialog } from '@/composables/State'
+import TheIcon from '@/components/TheIcon'
+
+const isHelp = useIsHelp()
+const isDialog = useIsDialog()
+</script>
--- project/components/TheHeader.vue
+++ project/components/TheHeader.vue
@@ -5,37 +5,16 @@
         🏞️ 画像結合・文字入れ
       </div>
     </div>
-    <div class="flex items-center gap-4">
-      <div>
-        update
-      </div>
-      <div>
-        contact
-      </div>
-    </div>
-    <div class="flex justify-end flex-1 gap-1 mr-4">
-      <div
-        class="flex items-center items-center"
-        @click="isHelp = !isHelp, isDialog = true"
-      >
-        <TheIcon
-          :path="mdiHelpCircleOutline"
-          size="28px"
-          class="mr-1"
-        />
-        <span>使い方</span>
-      </div>
+    <div class="flex justify-end flex-1 gap-2 mr-4">
+      <HelpBtn />
+      <VersionDate />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { mdiHelpCircleOutline } from '@mdi/js'
-import TheIcon from '@/components/TheIcon'
-import { useIsHelp, useIsDialog } from '@/composables/State'
-
-const isHelp = useIsHelp()
-const isDialog = useIsDialog()
+import HelpBtn from '@/components/HelpBtn'
+import VersionDate from '@/components/VersionDate'
 </script>
 
 <style lang="scss" scoped>
