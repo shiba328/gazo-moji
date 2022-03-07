5c1b80724ad48b43d02e097deeec18a7ca9b74b9
shiba mitsue <mitue.okamoto@gmail.com>
2022-03-07 12:56:30 +0900

    chore: add styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   srcDir: 'project/',
-  css: ['@/assets/main.scss'],
+  css: ['@/assets/main.scss']
 })
