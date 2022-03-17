8528c83771a54f16dcca4c2884c3ed68cc854b79
shiba mitsue <mitue.okamoto@gmail.com>
2022-03-17 16:47:32 +0900

    feat: add beforeunload

--- project/pages/index.vue
+++ project/pages/index.vue
@@ -9,6 +9,12 @@
 import TheHeader from '@/components/TheHeader.vue'
 import MainArea from '@/components/MainArea.vue'
 
+// 離脱確認
+window.addEventListener('beforeunload', (event) => {
+  event.preventDefault()
+  event.returnValue = ''
+})
+
 useMeta({
   title: '画像結合・文字入れ加工 | 資料作りに便利なオンラインツール',
   link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
@@ -25,13 +31,5 @@ useMeta({
   display: flex;
   flex-wrap: nowrap;
   flex-direction: column;
-
-  &.is-preview {
-    & :deep(.header),
-    & :deep(.main) {
-      filter: blur(3px);
-      overflow: hidden;
-    }
-  }
 }
 </style>
